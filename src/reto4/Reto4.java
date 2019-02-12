28d28687998038e637b06a97deb2ff0d447baea5
aldocesarmg <aldocesarmg@gmail.com>
2019-02-12 08:08:29 -0600

    Creacion del do while y del switch

--- src/reto4/Reto4.java
+++ src/reto4/Reto4.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package reto4;
 
-/**
- *
- * @author aldoc
- */
+import java.util.Scanner;
+
 public class Reto4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        char opcion;
+        double costo_boleto;
+        
+        do{
+            System.out.println("Indique el tipo de boleto");
+            opcion = sc.next().charAt(0);
+            switch (opcion){
+                case 'A':
+                    costo_boleto = 100.00;
+                    break;
+                case 'B':
+                    costo_boleto = 70.00;
+                    break;
+                case 'I':
+                    costo_boleto = 50.00;
+                    break;
+                case 'N':
+                    costo_boleto = 70.00;
+                    break;
+                default:
+                    System.out.println("Opción no disponible");
+            }
+            System.out.println("Costo del boleto es "+costo_boleto);
+            System.out.println("Otro boleto (s/n)?");
+            opcion = sc.next().charAt(0);
+        }while(opcion != 'n');
     }
     
 }
