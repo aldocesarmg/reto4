e5cb5f85df4b519e4b9a18d186112f39da3c870f
aldocesarmg <aldocesarmg@gmail.com>
2019-02-12 07:54:54 -0600

    Creacion de clase SegundoPrograma, metodos y atributos

---
+++ src/reto4/SegundoPrograma.java
@@ -0,0 +1,20 @@
+
+package reto4;
+
+public class SegundoPrograma {
+    public String nombre;
+    public int edad;
+    public double altura;
+    
+    public void saludar(){
+        
+    }
+    
+    public void despedirse(){
+        
+    }
+    
+    public void retirarse(){
+        
+    }
+}
